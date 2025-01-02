5e0c4dfb958cc6dcf592c78160d8f44315b5d7c2
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-02 15:00:31 +0530

    user id and password

---
+++ Main5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your ID: ");
+        String id = scanner.nextLine();
+
+        System.out.print("Enter your password: ");
+        String pass = scanner.nextLine();
+
+        if (id == "user001" && pass == "user_001") {
+            System.out.println("Welcome user, happy new year! 2K25");
+        } else {
+            System.out.println("Enter your credentials again.");
+        }
+
+        scanner.close();
+    }
+}
