7bbce28f0615d374809a9cf654fc914c8ffa6487
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-02 14:24:56 +0530

    greatest of three numbers

---
+++ Main3.java
@@ -0,0 +1,16 @@
+public class Main3 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        if (num1 > num2) {
+            System.out.println(num1 + " is the greatest");
+        } else if (num2 > num3) {
+            System.out.println(num2 + " is the greatest");
+        } else if (num3 > num1) {
+            System.out.println(num3 + " is the greatest");
+        } else {
+            System.out.println("all are equal");
+        }
+    }
+}
