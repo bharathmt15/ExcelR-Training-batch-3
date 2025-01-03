e9bd02b700d6ec68f10a61f3d441c14a5e3e2266
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-03 11:02:21 +0530

    user information code

---
+++ Main7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter your Phone_no: ");
+        String number = scanner.nextLine();
+
+        System.out.println("My name is: " + name);
+        System.out.println("My age is: " + age);
+        System.out.println("My number is: " + number);
+
+        scanner.close();
+    }
+}
