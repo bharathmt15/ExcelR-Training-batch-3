79a3f8d2f8adab40813c110eb9cd3081dfbc8765
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-03 11:07:25 +0530

    fixed git issue and user id code

--- Main6.java
+++ Main6.java
@@ -3,7 +3,7 @@ public class Main6 {
         String name = "Bharath";
         String password = "Bharath_001";
         while (name == "Bharath" && password == "Bharath_001") {
-            System.out.println(name + " i welcome you");
+            System.out.println(name + " i welcome you and happy new year");
             break;
         }
     }
