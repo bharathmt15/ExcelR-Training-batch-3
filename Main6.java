a255bfdff828d9db90e5764eae8483395e96936a
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-03 10:39:00 +0530

    using while loop for previous code

---
+++ Main6.java
@@ -0,0 +1,10 @@
+public class Main6 {
+    public static void main(String[] args) {
+        String name = "Bharath";
+        String password = "Bharath_001";
+        while (name == "Bharath" && password == "Bharath_001") {
+            System.out.println(name + " i welcome you");
+            break;
+        }
+    }
+}
