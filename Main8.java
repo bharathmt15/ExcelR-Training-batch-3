fd11fb6fbdc454659b4969f13813956101084e8b
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-03 11:36:14 +0530

    do while loop user data

--- Main8.java
+++ Main8.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main8 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+
+        String user = "Bharath";
+        String pass = "Bharath_123";
+
+        String username;
+        String password;
+        boolean login = true;
+
+        do {
+            System.out.print("Enter your username: ");
+            username = scanner.nextLine();
+
+            System.out.print("Enter your password: ");
+            password = scanner.nextLine();
+
+            if (username != user && password != pass) {
+                System.out.println("Login success");
+            } else {
+                System.out.println("Try again");
+            }
+        } while (!login);
+
+        System.out.println("Welcome to MRUH!");
+
+        scanner.close();
     }
 }
