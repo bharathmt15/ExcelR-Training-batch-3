d6c4de690888a9e913b558a626f01e69e389c57f
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-04 09:53:00 +0530

    using the display method

---
+++ Main9.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main9 {
+
+    public static void main(String[] args) {
+        Main9 main = new Main9();
+        int hardcodedBalance = 1000;
+        main.userdetails(1, hardcodedBalance, "1234567890");
+        main.display(1, hardcodedBalance, "1234567890");
+    }
+
+    void userdetails(int id, int balance, String ph_no) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your id: ");
+        id = sc.nextInt();
+        System.out.println("Enter your bank balance: ");
+        balance = 1000;
+        System.out.println("Enter your phone number: ");
+        ph_no = sc.next();
+        sc.close();
+    }
+
+    void display(int id, int balance, String ph_no) {
+        System.out.println("User details: ");
+        System.out.println("Your id is " + id);
+        System.out.println("Your bank balance is " + balance);
+        System.out.println("Your phone number is " + ph_no);
+    }
+}
