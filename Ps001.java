1eb0d2359cee8ebd22345893e4846fd60b40f938
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-08 11:29:54 +0530

    updated employee

--- Ps001.java
+++ Ps001.java
@@ -11,6 +11,13 @@ class Employee {
         this.address = "";
     }
 
+    Employee(int id, String firstname, String lastname, String address) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+    }
+
     public String getFullName() {
         return firstname + " " + lastname;
     }
@@ -30,24 +37,38 @@ class Employee {
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+}
 
-    class PartTimeEmployee extends Employee {
-        private double hoursworked;
-        private double amountPerHour;
+class PartTimeEmployee extends Employee {
+    private double hoursworked;
+    private double amountPerHour;
 
-    PartTimeEmployee(){
-        public PartTimeEmployee(int id , String firstname , String lastname , String address){
-        super(id,firstname,lastname,address);
+    PartTimeEmployee() {
+        super();
         this.hoursworked = 0;
         this.amountPerHour = 0;
-}
-}
     }
 
+    PartTimeEmployee(int id, String firstname, String lastname, String address, double hoursworked,
+            double amountPerHour) {
+        super(id, firstname, lastname, address);
+        this.hoursworked = hoursworked;
+        this.amountPerHour = amountPerHour;
+    }
+
+    public double calculateSalary() {
+        return hoursworked * amountPerHour;
+    }
+}
+
+public class Ps001 {
     public static void main(String[] args) {
         Employee emp = new Employee();
         emp.setFirstname("John");
         emp.setLastname("Doe");
         System.out.println("Full Name: " + emp.getFullName());
+
+        PartTimeEmployee partTimeEmp = new PartTimeEmployee(1, "Jane", "Smith", "123 Main St", 20, 15);
+        System.out.println("Part-Time Employee Salary: " + partTimeEmp.calculateSalary());
     }
 }
