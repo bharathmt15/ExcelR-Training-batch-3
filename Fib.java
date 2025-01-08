ecd57b190d11358b9484c491c13ab70408daeddf
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-08 11:39:45 +0530

    Fibonacci numbers

---
+++ Fib.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fib {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of Fibonacci terms to print: ");
+        int n = scanner.nextInt();
+        int first = 0, second = 1;
+
+        System.out.print("Fibonacci Series: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+        scanner.close();
+    }
+}
