f72d9b3fb5bc483437f171aa03a793c0f94a7313
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-08 11:36:28 +0530

    rounding to the nearest value

--- Main019.java
+++ Main019.java
@@ -6,7 +6,14 @@ public class Main019 {
         System.out.print("Enter a two-digit number: ");
         int number = scanner.nextInt();
         int remainder = number % 10;
-        int rounded = remainder < 5 ? number - remainder + 5 : number - remainder + 10;
+        int rounded;
+        if (remainder <= 2) {
+            rounded = number - remainder;
+        } else if (remainder <= 7) {
+            rounded = number - remainder + 5;
+        } else {
+            rounded = number - remainder + 10;
+        }
         System.out.println("Rounded number: " + rounded);
         scanner.close();
     }
