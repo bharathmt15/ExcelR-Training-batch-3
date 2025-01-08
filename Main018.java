96b8b4a57aa03779713cbbbe711fd35fce8f9a17
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-08 11:26:44 +0530

    minutes to years

---
+++ Main018.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main018 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter minutes: ");
+        long minutes = scanner.nextLong();
+        long years = minutes / (60 * 24 * 365);
+        long remainingDays = (minutes % (60 * 24 * 365)) / (60 * 24);
+        System.out.println(minutes + " minutes is approximately " + years + " years and " + remainingDays + " days.");
+        scanner.close();
+    }
+}
