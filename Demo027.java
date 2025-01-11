154376f7f5c021912460b1ce342fe77564bd6faa
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-11 11:38:06 +0530

    checking weather number is prime or not

---
+++ Demo027.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Demo027 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("number to check prime or not ");
+        int num = scanner.nextInt();
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        scanner.close();
+
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
