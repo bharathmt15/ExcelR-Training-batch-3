768b6ce00c9d4f358ba425220a6f44d6a906bce4
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-22 12:57:46 +0530

    leetcode problem in reversing a string

--- Lt002.java
+++ Lt002.java
@@ -1,20 +1,21 @@
-class Lt002 {
+import java.util.Scanner;
+
+class Lt001 {
     public int reverse(int x) {
-        int ans = 0;
-        int pre = 0;
+        int reversed = 0;
         while (x != 0) {
-            pre = ans;
-            ans = ans * 10 + x % 10;
-            if (ans / 10 != pre)
-                return 0;
-            x = x / 10;
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
         }
-        return ans;
+        return reversed;
     }
 
     public static void main(String[] args) {
-        Lt002 obj = new Lt002();
-        int result = obj.reverse(45);
-        System.out.println(result);
+        Scanner sc = new Scanner(System.in);
+        Lt001 obj = new Lt001();
+
+        int num = sc.nextInt();
+        System.out.println(obj.reverse(num));
+        sc.close();
     }
 }
