589bfe1e8184dc86048acac033bc753ec08462a3
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-23 19:01:33 +0530

    case checking of a character

---
+++ Demo037.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Demo037 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first number");
+        int num1 = sc.nextInt();
+        System.out.println("enter the second number");
+        int num2 = sc.nextInt();
+        System.out.println("enter the third number");
+        int num3 = sc.nextInt();
+        System.out.println("enter the fourth number");
+        int num4 = sc.nextInt();
+
+        if (num1 > num2) {
+            System.out.println(num1 + " is greater ");
+        } else if (num2 > num3) {
+            System.out.println(num2 + " is greater ");
+
+        } else if (num3 > num4) {
+            System.out.println(num3 + " is greater ");
+
+        } else {
+            System.out.println(num4 + " is greater ");
+        }
+        sc.close();
+    }
+}
---
+++ Demo038.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Demo038 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a letter to check it's case");
+        char word = sc.next().charAt(0);
+        if (Character.isUpperCase(word)) {
+            System.out.println(word + " is capital letter ");
+        } else if (Character.isLowerCase(word)) {
+            System.out.println(word + " is small letter");
+        } else {
+            System.out.println("it is not a character");
+        }
+        sc.close();
+    }
+}
