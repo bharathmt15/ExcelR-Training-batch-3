f88b41c53b9c0eb9697410fbe7343e02bf3cb9e6
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-23 19:10:47 +0530

    greatest among 2 numbers

--- Demo035.java
+++ Demo035.java
@@ -5,7 +5,7 @@ public class Demo035 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first number");
         int num1 = sc.nextInt();
-        System.out.println("enter the second number");
+        System.out.println("Enter the second number");
         int num2 = sc.nextInt();
         if (num1 > num2) {
             System.out.println(num1 + " is greater ");
