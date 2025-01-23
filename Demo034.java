b7dbbdde55037406d70e31d96757272fe751776d
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-23 18:46:17 +0530

    To check if it is even or odd

---
+++ Demo034.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Demo034 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number to check if is even or odd");
+        int val = sc.nextInt();
+        if (val % 2 == 0) {
+            System.out.println(val + " is a even number");
+        } else {
+            System.out.println(val + " is a odd number");
+
+        }
+        sc.close();
+
+    }
+}
