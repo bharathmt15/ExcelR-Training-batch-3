2feff8891fa9323edf101011f8abb399ddb1d088
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-23 19:12:12 +0530

    checking even or odd number

--- Demo034.java
+++ Demo034.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Demo034 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter a number to check if is even or odd");
+        System.out.println("Enter a number to check if is even or odd");
         int val = sc.nextInt();
         if (val % 2 == 0) {
             System.out.println(val + " is a even number");
