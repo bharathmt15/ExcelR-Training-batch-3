a8f6fc655607f6288c3885830bc40d868aa9352f
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-23 19:12:57 +0530

    greatest among 3 numbers

--- Demo036.java
+++ Demo036.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Demo036 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the first number");
+        System.out.println("Enter the first number");
         int num1 = sc.nextInt();
-        System.out.println("enter the second number");
+        System.out.println("Enter the second number");
         int num2 = sc.nextInt();
-        System.out.println("enter the third number");
+        System.out.println("Enter the third number");
         int num3 = sc.nextInt();
 
         if (num1 > num2) {
