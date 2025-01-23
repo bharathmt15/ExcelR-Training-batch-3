bcfcfd17bba6f42990181a4987ec96d19225deb0
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-23 19:09:04 +0530

    greatest number among 2 numbers

--- Demo035.java
+++ Demo035.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Demo035 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the first number");
+        System.out.println("Enter the first number");
         int num1 = sc.nextInt();
         System.out.println("enter the second number");
         int num2 = sc.nextInt();
