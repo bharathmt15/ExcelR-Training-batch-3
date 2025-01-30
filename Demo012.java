d5dcdbe55857d0e18868e502d4c662b79ad780a8
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-30 20:49:33 +0530

    Araa of the circle

---
+++ Demo012.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Demo012 {
+    void Area(int rad) {
+        double ca = Math.PI * rad * rad;
+        System.out.println("Area of Circle: " + ca);
+    }
+
+    public static void main(String[] args) {
+        Demo012 obj = new Demo012();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius: ");
+        int rad = sc.nextInt();
+        obj.Area(rad);
+        sc.close();
+    }
+}
