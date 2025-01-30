fa4899c2c02bda655d1b3bf3e569004ddc476fc2
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-30 20:55:42 +0530

    swapping of two numbers using a third variable

---
+++ Demo015.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Demo015 {
+    void swapNumber(int a, int b) {
+        System.out.println("Before Swapping:\na = " + a + "\nb = " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After Swapping:\na = " + a + "\nb = " + b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Demo015 obj = new Demo015();
+        System.out.println("Enter 2 numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        obj.swapNumber(a, b);
+        sc.close();
+    }
+}
