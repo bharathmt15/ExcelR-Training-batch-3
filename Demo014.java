8bb0fa27845197b2a826f2ce3c5a6e5911ec80f3
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-30 20:53:46 +0530

    Unary operators

---
+++ Demo014.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Demo014 {
+    void unary(int a) {
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(--a);
+        System.out.println(a--);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Demo014 obj = new Demo014();
+        System.out.print("Enter a number: ");
+        int a = sc.nextInt();
+        obj.unary(a);
+        sc.close();
+    }
+}
