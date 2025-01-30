37f3a7ac04cc6288f55d9becf38ead266976570a
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-30 20:57:05 +0530

    Palindrome of a number

---
+++ Demo016.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Demo016 {
+    public static void main(String[] args) {
+        PalindromeOf3 obj = new PalindromeOf3();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a three digit number: ");
+        int n = sc.nextInt();
+        System.out.println(obj.isPalindromeOf3Digits(n));
+        sc.close();
+
+    }
+}
+
+class PalindromeOf3 {
+    boolean isPalindromeOf3Digits(int n) {
+        return (n / 100) == (n % 10);
+    }
+}
