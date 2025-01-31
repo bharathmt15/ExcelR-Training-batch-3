5bab80edf0f5c02cdbae09ff2901047a309bcb84
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-01-31 12:53:00 +0530

    sum of the cubes of the digit

---
+++ Demo056.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Demo056 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a value ");
+        int val = sc.nextInt();
+        Sol obj = new Sol();
+        obj.sumsqr(val);
+        sc.close();
+    }
+}
+
+class Sol {
+    public void sumsqr(int a) {
+        int ans = 0;
+        if (a > 0) {
+            ans += a % 10;
+            ans = a * a * a;
+            a /= 10;
+            System.out.println(ans);
+        } else {
+            System.out.println(ans);
+        }
+    }
+}
