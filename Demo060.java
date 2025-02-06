87b22863d552b3c0adde7313a67d77e1263c3ea5
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-02-06 12:12:38 +0530

    Fibonacci series based on user input

--- Demo060.java
+++ Demo060.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 class Demo060 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+
         Fibonacci fib = new Fibonacci();
-        fib.printFibonacci(20);
+        fib.printFibonacci(num);
+        sc.close();
     }
 }
 
 class Fibonacci {
     void printFibonacci(int n) {
         int a = 0, b = 1;
-        System.out.print(n);
+        // System.out.print(n);
         for (int i = 1; i <= n; i++) {
             System.out.print(a + " ");
             int next = a + b;
