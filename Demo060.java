7ab49233e44afaaaf714756727e4f2006c2908ba
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2025-02-10 11:43:00 +0530

    Fibonacci series of a number

--- Demo060.java
+++ Demo060.java
@@ -15,7 +15,6 @@ class Demo060 {
 class Fibonacci {
     void printFibonacci(int n) {
         int a = 0, b = 1;
-        // System.out.print(n);
         for (int i = 1; i <= n; i++) {
             System.out.print(a + " ");
             int next = a + b;
